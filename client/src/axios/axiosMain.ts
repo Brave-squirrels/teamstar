248055dc97498154b12f31573ba87fcba59cda4d
Olek <aleksander.atamanczuk@gmail.com>
2021-04-20 18:35:12 +0200

    refactor: disable react tools on deploy

--- client/src/axios/axiosMain.ts
+++ client/src/axios/axiosMain.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const address = process.env.SERVER_ADDRESS || "http://localhost:5000";
+
 const instance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: address,
   headers: {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE",
