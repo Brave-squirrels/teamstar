d078baa06a707f9150ee33c98324cb683b13b955
Cyprian Zamiara <veryyapeee@gmail.com>
2021-04-20 18:15:43 +0200

    feature: routing with login

--- client/src/containers/landingPage/landingPage.tsx
+++ client/src/containers/landingPage/landingPage.tsx
@@ -1,11 +1,69 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+import { Button, Modal } from "react-bootstrap";
+import MyTextInput from "components/form/MyTextInput";
 
-const landingPage = () => {
+import { loginUserFetch } from "reduxState/user/loginUser";
+interface LoginData {
+  email: string;
+  password: string;
+}
+
+const LandingPage = () => {
+  const dispatch = useDispatch();
+
+  const validationSchema = Yup.object({
+    email: Yup.string().required("Email pls"),
+    password: Yup.string().required("Password pls"),
+  });
+
+  const sampleValues: LoginData = {
+    email: "",
+    password: "",
+  };
+
+  const handleFormSubmit = (data: any) => {
+    dispatch(loginUserFetch(data));
+  };
+
   return (
-    <div>
-      <span>Hello, login page here</span>
-    </div>
+    <Modal.Dialog>
+      <Modal.Header>
+        <Modal.Title>Login</Modal.Title>
+      </Modal.Header>
+      <Formik
+        validationSchema={validationSchema}
+        enableReinitialize
+        initialValues={sampleValues}
+        onSubmit={(values) => handleFormSubmit(values)}
+      >
+        {({ handleSubmit, isValid, isSubmitting, dirty }) => (
+          <Form
+            className={"form-group"}
+            onSubmit={handleSubmit}
+            autoComplete="off"
+          >
+            <Modal.Body>
+              <MyTextInput name="email" placeholder="E-mail" />
+              <MyTextInput name="password" placeholder="********" />
+            </Modal.Body>
+            <Modal.Footer>
+              <Button
+                disabled={isSubmitting || !dirty || !isValid}
+                type="submit"
+                variant="success"
+                className="pull-right"
+              >
+                Submit
+              </Button>
+            </Modal.Footer>
+          </Form>
+        )}
+      </Formik>
+    </Modal.Dialog>
   );
 };
 
-export default landingPage;
+export default LandingPage;
--- client/src/hoc/navbar/navbar.tsx
+++ client/src/hoc/navbar/navbar.tsx
@@ -1,33 +1,38 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { NavLink } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
 
-const navbar = () => {
+import { logout } from "reduxState/user/loginUser";
+
+const Navigation = () => {
+  const dispatch = useDispatch();
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Brand to="/home" as={NavLink}>
+        Hackathon
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-          <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
+          <Nav.Link to="/testInput" as={NavLink}>
+            Test input
+          </Nav.Link>
+          <NavDropdown title="Sample dropdown" id="collasible-nav-dropdown">
+            <NavDropdown.Item to="/home" as={NavLink}>
+              Sample dropdown link
             </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
             <NavDropdown.Divider />
             <NavDropdown.Item href="#action/3.4">
-              Separated link
+              Sample separator link
             </NavDropdown.Item>
           </NavDropdown>
         </Nav>
         <Nav>
-          <Nav.Link href="#deets">More deets</Nav.Link>
-          <Nav.Link eventKey={2} href="#memes">
-            Dank memes
+          <Nav.Link href="/" onClick={() => dispatch(logout())}>
+            Logout
           </Nav.Link>
         </Nav>
       </Navbar.Collapse>
@@ -35,4 +40,4 @@ const navbar = () => {
   );
 };
 
-export default navbar;
+export default Navigation;
