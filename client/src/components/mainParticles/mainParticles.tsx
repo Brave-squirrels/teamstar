3e4af1428a9274ebe410abfb70ff871e6bd72567
Michal <michal.smalira@gmail.com>
2021-04-22 22:55:44 +0200

    custom aprticles

--- client/src/components/mainParticles/mainParticles.tsx
+++ client/src/components/mainParticles/mainParticles.tsx
@@ -3,6 +3,53 @@ import styles from "./mainParticles.module.scss";
 import React from "react";
 
 const mainParticles = () => {
-  return <Particles className={styles.wrapper} />;
+  return (
+    <Particles
+      className={styles.wrapper}
+      params={{
+        particles: {
+          number: {
+            value: 60,
+            density: {
+              enable: true,
+              value_area: 1500,
+            },
+          },
+          line_linked: {
+            enable: true,
+            opacity: 0.02,
+          },
+          move: {
+            direction: "right",
+            speed: 0.05,
+          },
+          size: {
+            value: 1,
+          },
+          opacity: {
+            anim: {
+              enable: true,
+              speed: 1,
+              opacity_min: 0.05,
+            },
+          },
+        },
+        interactivity: {
+          events: {
+            onclick: {
+              enable: true,
+              mode: "push",
+            },
+          },
+          modes: {
+            push: {
+              particles_nb: 1,
+            },
+          },
+        },
+        retina_detect: true,
+      }}
+    />
+  );
 };
-export default mainParticles
+export default mainParticles;
