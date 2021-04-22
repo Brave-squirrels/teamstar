1fcfe32b61e468ead245c8977324e6deda5a9699
Olek <aleksander.atamanczuk@gmail.com>
2021-04-23 01:46:43 +0200

    feature: settings panel

--- client/src/containers/user/settings/settings.tsx
+++ client/src/containers/user/settings/settings.tsx
@@ -2,13 +2,58 @@ import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 import FormStructure from "containers/form/formStructure";
+import {
+  Button,
+  Card,
+  Col,
+  Container,
+  Form,
+  Modal,
+  Row,
+} from "react-bootstrap";
 
 import { changeNameFetch } from "reduxState/user/changeName";
 import { changePasswordFetch } from "reduxState/user/changePassword";
 import { RootState } from "reduxState/store";
 import { mutateToAxios } from "utils/onChangeForm";
 
+function MyVerticallyCenteredModal(props: any) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header
+        closeButton
+        className="flex-column-reverse align-items-center"
+      >
+        <p>Type new user name and current password</p>
+
+        <Modal.Title id="contained-modal-title-vcenter">
+          Change your user name
+        </Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body>
+        <Form.Label>USER NAME</Form.Label>
+        <Form.Control type="text" placeholder="user_name"></Form.Control>
+        <br />
+        <Form.Label>CURRENT PASSWORD</Form.Label>
+        <Form.Control type="text"></Form.Control>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Cancel</Button>
+        <Button onClick={props.onHide}>Done</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
 const Settings = () => {
+  const [modalShow, setModalShow] = React.useState(false);
+
   const dispatch = useDispatch();
   const changeName = useSelector((state: RootState) => state.changeName);
   const userInfo = useSelector((state: RootState) => state.loginUser);
@@ -41,6 +86,21 @@ const Settings = () => {
         maxLength: 50,
       },
       error: "Name should be between 4 and 50 characters long",
+      touched: false,
+      valid: false,
+    },
+    email: {
+      val: "",
+      type: "text",
+      inputType: "input",
+      placeholder: "Email",
+      label: "Email",
+      validation: {
+        required: true,
+        minLength: 4,
+        maxLength: 50,
+      },
+      error: "",
       touched: false,
       valid: false,
     },
@@ -111,25 +171,60 @@ const Settings = () => {
   };
 
   return (
-    <div>
-      <FormStructure
-        state={changeNameForm}
-        setState={setChangeNameForm}
-        btnText="Change"
-        title="Change name"
-        submitted={handleChangeName}
-        spinner={changeName.loading}
+    <>
+      <Container className="mt-5">
+        <h2 className="text-light">MY ACCOUNT</h2>
+        <Card bg="secondary" text="white">
+          <Card.Body>
+            <Card.Subtitle>USER NAME</Card.Subtitle>
+            <Row md="2" className="mb-4">
+              <Card.Text className="w-50%">user_name</Card.Text>
+              <div className="text-right">
+                <Button onClick={() => setModalShow(true)}>Edit</Button>
+              </div>
+            </Row>
+
+            <Card.Subtitle>EMAIL ADDRESS</Card.Subtitle>
+            <Row md="2" className="mb-4">
+              <Card.Text className="w-50%">
+                ****@mail.com<Card.Link href="#">show</Card.Link>
+              </Card.Text>
+              <div className="text-right">
+                <Button>Edit</Button>
+              </div>
+            </Row>
+          </Card.Body>
+        </Card>
+        <hr />
+        <Button>Change password</Button>
+        <hr />
+        <Button variant="danger">Delete account</Button>
+        {/* <FormStructure
+              state={changeNameForm}
+              setState={setChangeNameForm}
+              btnText="Change"
+              title="Change name"
+              submitted={handleChangeName}
+              spinner={changeName.loading}
+              direction="row"
+            /> */}
+
+        {/* <FormStructure
+              state={changePasswordForm}
+              setState={setChangePasswordForm}
+              btnText="Change"
+              title="Change password"
+              submitted={handleChangePassword}
+              spinner={changePassword.loading}
+              checkPass={true}
+              direction="row"
+            /> */}
+      </Container>
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
       />
-      <FormStructure
-        state={changePasswordForm}
-        setState={setChangePasswordForm}
-        btnText="Change"
-        title="Change password"
-        submitted={handleChangePassword}
-        spinner={changePassword.loading}
-        checkPass={true}
-      />
-    </div>
+    </>
   );
 };
 
