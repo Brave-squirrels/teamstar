5b6fad420a15f6b0c7c2e69c4c101850afe0bd3f
Michal <michal.smalira@gmail.com>
2021-04-22 10:21:27 +0200

    register bugfix

--- server/src/users/validateUser.ts
+++ server/src/users/validateUser.ts
@@ -6,6 +6,7 @@ import User from "../../interfaces/user.interface";
 export default function validateUser(user: User) {
   const schema = Joi.object({
     name: Joi.string().min(4).max(50).required(),
+    password: Joi.string().required(),
     confirmPassword: Joi.string().min(8).max(255).required(),
     email: Joi.string().min(5).max(50).required().email(),
   });
