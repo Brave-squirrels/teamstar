19182ece21017053028b3b5b8ded6a0fd91d4080
Maciej Chojak <maciekch7991@gmail.com>
2021-04-23 23:27:24 +0200

    feature: create new sidebar

--- client/src/containers/team/sidebar/sidebar.tsx
+++ client/src/containers/team/sidebar/sidebar.tsx
@@ -5,12 +5,19 @@ const Sidebar = () => {
   const user = {
     name: "asdsas",
     teams: [
-      { id: "1231233", teamName: "team1" },
-      { id: "1231233", teamName: "team1" },
+      { teamId: "1231233", teamName: "team1" },
+      { teamId: "123123333", teamName: "team2" },
+      { teamId: "123d3333", teamName: "team3" },
     ],
   };
 
   return <div className={styles.sidebar}>
+      <h3>Przerwa</h3>
+      <div>od 12:45:12</div>
+      <div>do 20:20:30</div>
+      <ul className={styles.menu}>
+      {user.teams.map(((team : any) => <li key={team.teamId}>{team.teamName}</li>))}
+      </ul>
       
   </div>;
 };
