3265f2f10b234cf2594852da05aa8be2a5328f4c
Cyprian Zamiara <veryyapeee@gmail.com>
2021-04-23 23:28:07 +0200

    working on calendar styles

--- client/src/containers/calendar/calendar.tsx
+++ client/src/containers/calendar/calendar.tsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import moment from "moment";
+
+import styles from "./calendar.module.scss";
+
+const localizer = momentLocalizer(moment);
+
+const events: any = [
+  {
+    id: 14,
+    title: "Today",
+    start: new Date(new Date().setHours(new Date().getHours() - 3)),
+    end: new Date(new Date().setHours(new Date().getHours() + 3)),
+  },
+];
 
 const calendar = () => {
-  return <div></div>;
+  return (
+    <div className={styles.wrapper}>
+      <Calendar
+        localizer={localizer}
+        startAccessor="start"
+        endAccessor="end"
+        events={events}
+      />
+    </div>
+  );
 };
 
 export default calendar;
