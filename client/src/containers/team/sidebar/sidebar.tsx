aa1740f8e2723fcf26b96a56c57e8c6025f6edbf
Maciej Chojak <maciekch7991@gmail.com>
2021-04-23 22:39:01 +0200

    feature: view of singleTeam

---
+++ client/src/containers/team/sidebar/sidebar.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./sidebar.module.scss";
+
+const Sidebar = () => {
+  const user = {
+    name: "asdsas",
+    teams: [
+      { id: "1231233", teamName: "team1" },
+      { id: "1231233", teamName: "team1" },
+    ],
+  };
+
+  return <div className={styles.sidebar}>
+      
+  </div>;
+};
+
+export default Sidebar
