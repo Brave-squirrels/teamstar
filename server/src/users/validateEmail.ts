a1e12f843b9e00ca9ebd8060757d2430ca377db2
Cyprian Zamiara <veryyapeee@gmail.com>
2021-04-23 17:06:37 +0200

    bugfix

--- server/src/users/validateEmail.ts
+++ server/src/users/validateEmail.ts
@@ -3,7 +3,7 @@ import Joi from "joi";
 export default function validateEmail(email: { email: string }) {
   const schema = Joi.object({
     email: Joi.string().min(5).max(50).required().email(),
-    newEmail: Joi.string().min(5).max(50).required().email(),
+    newEmail: Joi.string().min(5).max(50).email(),
   });
 
   return schema.validate(email);
