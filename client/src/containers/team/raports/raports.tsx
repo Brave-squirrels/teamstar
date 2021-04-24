975ea76b52f8b6085d9f4f3da0187425dab7f080
Maciej Chojak <maciekch7991@gmail.com>
2021-04-24 12:48:26 +0200

    feature: change modal reports

--- client/src/containers/team/raports/raports.tsx
+++ client/src/containers/team/raports/raports.tsx
@@ -7,14 +7,23 @@ const Raports = (props: any) => {
   const raports = useSelector(
     (state: RootState) => state.getRaports.raportData
   );
+  const user = useSelector((state: RootState) => state.loginUser);
+const dupa = () => console.log(user)
   return (
     <div className={styles.wrapper}>
       {raports.length > 0 && (
         <>
           {raports.map((raport: any) => (
             <div className={styles.container}>
-              <div>{raport.name}</div>
-              <div>{raport.author.name}</div>
+              <div className={styles.raportColumn}>
+                <div className={styles.raportTitle}>{raport.name}</div>
+                <div>{raport.date.match(/[0-9]{4}-[0-9]{2}-[0-9]{2}/)}</div>
+              </div>
+              <div className={styles.raportColumn}>
+                <div className={styles.reportAuthor}>{raport.author.name}</div>
+                <div onClick={dupa} className={styles.deleteRaport}>Delete Raport</div>
+              </div>
+
               <p>{raport.description}</p>
             </div>
           ))}
